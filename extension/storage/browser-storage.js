bd37492068eb53f1154a1b6aeb9f7015d6b7f782
JM <Justmarsh88@gmail.com>
2026-09-15 17:30:05 -0400

    refactor: consolidate extension storage exports

---
+++ extension/storage/browser-storage.js
@@ -0,0 +1,2 @@
+export { createWatchlistStore, WATCHLIST_STORAGE_KEY, MAX_WATCHLIST_RECORDS } from './watchlist.js';
+export { createCostPresetStore, COST_PRESET_STORAGE_KEY, DEFAULT_COST_PRESET_STORAGE_KEY, MAX_COST_PRESETS } from './cost-presets.js';
